c79f1c1372c54e68f63f08047096bf9ea9653e00
Mahdiyeh <www.mahdiyeh.mazani@gmail.com>
2023-02-20 22:14:33 +0330

    Add files via upload

--- Hop/hop.py
+++ Hop/hop.py
@@ -1,1 +1,23 @@
+#for using random method, we should import this function
+import random
+# the first number
+First=random.randint(0, 10)
+while First%5==0:
+    First=random.randint(0, 10)
+print('The first number:'+str(First))
+#User number that is True
+UserNumberT=First+1
+while True:
+#User number that is received
+    UserNumberR=int(input('The correct answer:'))
+    if UserNumberR==UserNumberT:
+        print('Correct!')            
+        print(UserNumberT+1)  
+    else:
+            
+        print('Wrong')
+        print('The correct answer:'+str(UserNumberT))
+        break
+    UserNumberT=UserNumberR+2
 
+    
