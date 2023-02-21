a294fc0332472552943178ec477b838f380e9d6e
Mahdiyeh <www.mahdiyeh.mazani@gmail.com>
2023-02-21 20:07:12 +0330

    Add files via upload

--- Hop/hop.py
+++ Hop/hop.py
@@ -10,13 +10,21 @@ UserNumberT=First+1
 while True:
     if UserNumberT%5 !=0:
 #User number that is received
-        UserNumberR=int(input('The correct answer:'))
-        if UserNumberR==UserNumberT:
-            print('Correct!')
-            if (UserNumberT+1)%5==0:
-                print("Hop")
+        UserNumberR=input('The correct answer:')
+        if UserNumberR !="hop" and UserNumberR !="Hop":
+            UserNumberR=int(UserNumberR)
+            if UserNumberR==UserNumberT:
+                print('Correct!')
+                if (UserNumberT+1)%5==0:
+                    print("Hop")
+                else:
+                    print(UserNumberT+1)  
+        
             else:
-                print(UserNumberT+1)  
+            
+                print('Wrong')
+                print('The correct answer:'+str(UserNumberT))
+                break
         else:
             
             print('Wrong')
