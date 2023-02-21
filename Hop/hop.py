4169ae9d49d16266889a83cd7eb92de91db0db8c
Mahdiyeh <www.mahdiyeh.mazani@gmail.com>
2023-02-21 20:52:45 +0330

    Add files via upload

--- Hop/hop.py
+++ Hop/hop.py
@@ -2,6 +2,10 @@
 import random
 # the first number
 First=random.randint(0, 10)
+#user's name
+Name=input('Ur name:')
+#user's score
+Score=0
 while First%5==0:
     First=random.randint(0, 10)
 print('The first number:'+str(First))
@@ -15,6 +19,7 @@ while True:
             UserNumberR=int(UserNumberR)
             if UserNumberR==UserNumberT:
                 print('Correct!')
+                Score=Score+1
                 if (UserNumberT+1)%5==0:
                     print("Hop")
                 else:
@@ -24,22 +29,28 @@ while True:
             
                 print('Wrong')
                 print('The correct answer:'+str(UserNumberT))
+                print('Ur score:'+str(Score))
+    
                 break
         else:
             
             print('Wrong')
             print('The correct answer:'+str(UserNumberT))
+            print('Ur score:'+str(Score))
+    
             break
     else:
         UserNumberR=input('The correct answer:')
         if UserNumberR=="hop" or UserNumberR=="Hop":
-            print('Correct!')            
+            print('Correct!') 
+            Score=Score+1
             print(UserNumberT+1) 
         else:
             
             print('Wrong')
-            print('The correct answer:hop')
+            print('The correct answer:'+str(UserNumberT))
+            print('Ur score:'+str(Score))
+    
             break 
     UserNumberT=UserNumberT+2
-
-    
+    print('Ur score:'+str(Score))
