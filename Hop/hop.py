6e344a0021831e9d9ddf49b315705ed59d55f47d
Mahdiyeh <www.mahdiyeh.mazani@gmail.com>
2023-02-23 15:16:09 +0330

    Add files via upload

--- Hop/hop.py
+++ Hop/hop.py
@@ -1,56 +1,91 @@
 #for using random method, we should import this function
 import random
-# the first number
-First=random.randint(0, 10)
+
 #user's name
 Name=input('Ur name:')
-#user's score
-Score=0
-while First%5==0:
+
+def Game():
+    #user's score
+    Score=0
+    # the first number
     First=random.randint(0, 10)
-print('The first number:'+str(First))
-#User number that is True
-UserNumberT=First+1
-while True:
-    if UserNumberT%5 !=0:
-#User number that is received
-        UserNumberR=input('The correct answer:')
-        if UserNumberR !="hop" and UserNumberR !="Hop":
-            UserNumberR=int(UserNumberR)
-            if UserNumberR==UserNumberT:
-                print('Correct!')
-                Score=Score+1
-                if (UserNumberT+1)%5==0:
-                    print("Hop")
+    while First%5==0:
+        First=random.randint(0, 10)
+    print('The first number:'+str(First))
+    #User number that is True
+    UserNumberT=First+1
+    while True:
+        if UserNumberT%5 !=0:
+    #User number that is received
+            UserNumberR=input('The correct answer:')
+            if UserNumberR !="hop" and UserNumberR !="Hop":
+                UserNumberR=int(UserNumberR)
+                if UserNumberR==UserNumberT:
+                    print('Correct!')
+                    Score=Score+1
+                    if (UserNumberT+1)%5==0:
+                        print("Hop")
+                    else:
+                        print(UserNumberT+1)  
+            
                 else:
-                    print(UserNumberT+1)  
+                
+                    print('Wrong')
+                    if UserNumberT%5 !=0:
+                        print('The correct answer:'+str(UserNumberT))
+                    else:
+                        print('Hop')
+                    print('Do you want to play again?')
+                    #Answer is yes or no
+                    Answer=input('y or n?')
+                    if Answer=="y":
+                        Score=0
+                        Game()
+                    else:
+                        print('Ur score:'+str(Score))
+                        return
+
+                    
         
+                    
             else:
-            
+                
                 print('Wrong')
-                print('The correct answer:'+str(UserNumberT))
-                print('Ur score:'+str(Score))
-    
-                break
+                if UserNumberT%5 !=0:
+                    print('The correct answer:'+str(UserNumberT))
+                else:
+                    print('Hop')
+                print('Do you want to play again?')
+                    #Answer is yes or no
+                Answer=input('y or n?')
+                if Answer=="y":
+                    Score=0
+                    Game()
+                else:
+                    print('Ur score:'+str(Score))
+                    return
         else:
-            
-            print('Wrong')
-            print('The correct answer:'+str(UserNumberT))
-            print('Ur score:'+str(Score))
-    
-            break
-    else:
-        UserNumberR=input('The correct answer:')
-        if UserNumberR=="hop" or UserNumberR=="Hop":
-            print('Correct!') 
-            Score=Score+1
-            print(UserNumberT+1) 
-        else:
-            
-            print('Wrong')
-            print('The correct answer:'+str(UserNumberT))
-            print('Ur score:'+str(Score))
-    
-            break 
-    UserNumberT=UserNumberT+2
-    print('Ur score:'+str(Score))
+            UserNumberR=input('The correct answer:')
+            if UserNumberR=="hop" or UserNumberR=="Hop":
+                print('Correct!') 
+                Score=Score+1
+                print(UserNumberT+1) 
+            else:
+                
+                print('Wrong')
+                if UserNumberT%5 !=0:
+                    print('The correct answer:'+str(UserNumberT))
+                else:
+                    print('Hop')
+                print('Do you want to play again?')
+                    #Answer is yes or no
+                Answer=input('y or n?')
+                if Answer=="y":
+                    Score=0
+                    Game()
+                else:
+                    print('Ur score:'+str(Score))
+                    return
+        UserNumberT=UserNumberT+2
+#call function
+Game()
